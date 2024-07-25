3d712defe1b07b09a044569e36f40914f5012dc3
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2024-07-25 11:31:18 +0900

    Remove unused config

--- server-python/cgos/app/config.py
+++ server-python/cgos/app/config.py
@@ -60,8 +60,6 @@ class Configs:
     provisionalAge: float
     establishedAge: float
     killFile: str
-    tools_dir: str
-    bin_dir: str
     leeway: int
     anchor_match_rate: float
     badUsersFile: str
@@ -114,8 +112,6 @@ class Configs:
         self.provisionalAge = float(cfg["provisionalAge"])
         self.establishedAge = float(cfg["establishedAge"])
         self.killFile = str(cfg["killFile"])
-        self.tools_dir = str(cfg["tools_dir"])
-        self.bin_dir = str(cfg["bin_dir"])
         self.anchor_match_rate = float(cfg.get("anchor_match_rate", "0.10"))
         self.badUsersFile = str(cfg["bad_users_file"])
         self.moveIntervalBetweenSave = int(cfg["moveIntervalBetweenSave"])
